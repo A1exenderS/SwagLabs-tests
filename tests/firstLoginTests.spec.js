5699e93c81860eec277d93a7ed30d0fc4f404793
A1exenderS <vovnjanko.sasha2109@gmail.com>
2024-03-12 11:18:30 +0200

    Moving fixtures to the beginning of tests

--- tests/firstLoginTests.spec.js
+++ tests/firstLoginTests.spec.js
@@ -2,34 +2,39 @@ const { test, expect } = require("@playwright/test");
 
 test.describe("two simple tests", () => {
   test.beforeEach(async ({ page }) => {
+    const emailInput = page.getByPlaceholder("Username");
+    const passwordInput = page.getByPlaceholder("Password");
+    const loginButton = page.locator("#login-button");
+
     await page.goto("https://www.saucedemo.com/");
-    await page.getByPlaceholder("Username").fill("standard_user");
-    await page.getByPlaceholder("Password").fill("secret_sauce");
-    await page.click("#login-button");
+    await emailInput.fill("standard_user");
+    await passwordInput.fill("secret_sauce");
+    await loginButton.click();
   });
 
   test("Test 1 - Perform Login", async ({ page }) => {
-    await expect(page.locator(".title")).toBeVisible;
-    const inventoryItem = await page.$$(".inventory_item");
-    expect(inventoryItem.length).toBeGreaterThan(1);
+    const productTitle = page.locator(".title");
+    const inventoryItem = page.locator(".inventory_item");
+
+    expect(await productTitle).toBeVisible;
+    expect(await inventoryItem.count()).toBeGreaterThan(1);
   });
 
   test("Test 2 - Add product to the cart", async ({ page }) => {
-    await page.click("#add-to-cart-sauce-labs-backpack");
+    const addToCartSLBButton = page.locator("#add-to-cart-sauce-labs-backpack");
+    const shoppingCartBadge = page.locator(".shopping_cart_badge");
+    const cartItem = page.textContent(".inventory_item_name");
+    const shoppingCartLink = page.locator(".shopping_cart_link");
 
-    const shoppingCartBadgeText = page
-      .locator(".shopping_cart_badge")
-      .textContent();
+    await addToCartSLBButton.click();
 
-    expect(await shoppingCartBadgeText).toBe("1");
+    expect(await shoppingCartBadge.textContent()).toBe("1");
 
-    await page.click(".shopping_cart_link");
-    const cartItem = await page.textContent(".inventory_item_name");
-    expect(cartItem).toBe("Sauce Labs Backpack");
+    await shoppingCartLink.click();
+    expect(await cartItem).toBe("Sauce Labs Backpack");
 
     await page.click("#remove-sauce-labs-backpack");
 
-    const shoppingCartBadge = page.locator(".shopping_cart_badge");
     expect(await shoppingCartBadge.count()).toBe(0);
   });
 
